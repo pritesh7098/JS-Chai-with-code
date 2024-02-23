58804c8585927eedb547df3ac32c7d3bc252327b
Pritesh Dhanad <97663187+pritesh7098@users.noreply.github.com>
2024-02-23 12:06:03 +0000

    Datatypes conversion and operation done

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,15 @@
+
+// ************** Operations *****************//
+
+let str1="heyy"
+let str2="pritesh"
+
+let str3=str1+ str2;
+//console.log( str3)
+
+
+console.log("1"+2); // string first 
+console.log(1+1+"2"); //number first 
+
+console.log("1"+2+2);  // string first
+console.log(3+2+"2"+"2");//number first 
