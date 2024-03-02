25323c75fc356cd84df16a82315162b9fc6716b0
Pritesh Dhanad <97663187+pritesh7098@users.noreply.github.com>
2024-03-02 12:25:43 +0000

    done with the functions

--- 03_Basics/01_function.js
+++ 03_Basics/01_function.js
@@ -7,5 +7,27 @@ return `${username} just logged in `
 }
 
 
-console.log(regularUser("pritesh")); 
+//console.log(regularUser("pritesh")); 
 
+
+// rest operator usecase 
+
+function myValues(...num3)
+{
+return num3;;
+
+}
+
+//console.log(myValues(200,400,600,500,400));
+
+
+const myArr = [1,2,3,4]
+
+function myarr(parameter)
+{
+
+return parameter[2]
+
+}
+
+//console.log(myarr(myArr));
