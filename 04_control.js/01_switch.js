56d4fd4ccdd2deed7b9a2b33d376b9e527eb1df1
Pritesh Dhanad <97663187+pritesh7098@users.noreply.github.com>
2024-03-05 13:59:52 +0000

    completed controls

---
+++ 04_control.js/01_switch.js
@@ -0,0 +1,16 @@
+const myname = "pritesh"
+
+switch (myname) {
+    case "ram":
+        console.log("my name is ram");
+        break;
+    case "shyam":
+        console.log("my name is shyam");
+        break;
+    case "pritesh":
+        console.log("my name is pritesh");
+        break;
+
+    default:
+        break;
+}
